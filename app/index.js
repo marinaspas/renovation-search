5ee1ea359d57cc02bd0f5e3d8ae5dcf2c506ce27
Nick McCurdy <nick@nickmccurdy.com>
2019-09-17 07:05:36 -0400

    Use hashHistory for GitHub Pages support

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Router, Route, IndexRoute, browserHistory, Link} from 'react-router'
+import {Router, Route, IndexRoute, hashHistory, Link} from 'react-router'
 import AppLayout from './layouts/AppLayout'
 import Companies from './components/Companies'
 import CompanyProfile from './components/CompanyProfile'
 
 ReactDOM.render((
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path="/" component={AppLayout}>
       <IndexRoute component={Companies} />
       <Route path="/profile/:id" component={CompanyProfile} />
